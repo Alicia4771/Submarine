c3fc5fb41be42924ce735df9fcd7df53df50e5a2
Sota Tsukada <113979340+sotyan@users.noreply.github.com>
2025-10-26 03:31:56 +0900

    スコア加算と敵船オブジェクトの消去
    弾と敵船がぶつかった時の処理を追加しました。
    - スコアの加算
    - 敵船オブジェクトの消去（ヒエラルキーからとDataManagerのリストから）

--- SubmarineWar/Assets/Scripts/BulletControler.cs
+++ SubmarineWar/Assets/Scripts/BulletControler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BulletControler : MonoBehaviour
 {
@@ -17,10 +18,26 @@ public class BulletControler : MonoBehaviour
   // オブジェクト衝突時に呼び出される関数
   private void OnCollisionEnter(Collision collision)
   {
+    // EnemyShipに衝突した時
     if (collision.gameObject.CompareTag("EnemyShip"))
     {
-      Debug.Log("EnemyShipに衝突しました。");
+      string enemyName = collision.gameObject.name;
+      Debug.Log($"EnemyShipに衝突しました。名前: {enemyName}");
+      // スコアを加算
+      DataManager.AddScore(100);
+      // スコアを出力
+      Debug.Log($"現在のスコア:{DataManager.GetScore()}");
+      // EnemyShipオブジェクトの削除
+      Destroy(collision.gameObject);
+      DataManager.DeleteEnemyShip(enemyName);
+      // // 確認のため、敵船リストを表示
+      // List<string> enemyShips = DataManager.GetEnemyShipList();
+      // for (int i = 0; i < enemyShips.Count; i++)
+      // {
+      //   Debug.Log(enemyShips[i]);
+      // }
     }
+    // 壁に衝突した時
     else
     {
       Debug.Log("壁に衝突しました。");
