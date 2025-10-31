8ac1a846afc71c40a258f5dddb430a40d039b142
Alicia4771 <rinn1204@icloud.com>
2025-10-31 09:39:16 +0900

    敵船のうち、ソナーに表示するオブジェクトの情報だけをまとめる処理を追加

--- SubmarineWar/Assets/Scripts/SonarScript.cs
+++ SubmarineWar/Assets/Scripts/SonarScript.cs
@@ -7,9 +7,12 @@ public class SonarScript : MonoBehaviour
     private Vector3 center_point;
 
     private float sonar_radius = 1; // 潜水艦のソナーの半径
+    private float sonar_search_radius = 100;
 
     private List<float[]> EnemyShipList;
     private List<float[]> TorpedoList;
+    private List<Vector2> displayEnemyShipList;
+    private List<Vector2> displayTorpedoList;
 
 
 
@@ -21,6 +24,20 @@ public class SonarScript : MonoBehaviour
     void Update()
     {
         EnemyShipList = DataManager.GetEnemyShipDistanceList();
-        TorpedoList = DataManager.GetTorpedoList();
+        TorpedoList = DataManager.GetTorpedoDistanceList();
+
+        for (int i = 0; i < EnemyShipList.Count; i++)
+        {
+            if (EnemyShipList[i][2] < sonar_search_radius)
+            {
+                Vector2 direction = new Vector2(EnemyShipList[i][0], EnemyShipList[i][1]).normalized; // 正規化して方角をセット
+                direction = direction * (EnemyShipList[i][2] / sonar_search_radius);                // 潜水艦との距離で位置を調整
+                direction *= sonar_radius;            // ソナーの半径に合わせる、
+
+                displayEnemyShipList.Add(direction);
+            }
+        }
+
+
     }
 }
