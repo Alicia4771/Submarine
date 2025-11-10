b27d8ccbc95a876771183a23e73fbb7478d6e8f4
Alicia4771 <rinn1204@icloud.com>
2025-11-10 10:58:46 +0900

    実際に潜水艦を動かす処理を追加

--- SubmarineWar/Assets/Scripts/SubmarineMoveSample.cs
+++ SubmarineWar/Assets/Scripts/SubmarineMoveSample.cs
@@ -6,7 +6,10 @@ public class SubmarineMoveSample : MonoBehaviour
 
     private float speed;    // 潜水艦の速度
     private float depth;    // 潜水艦の深さ
-    private float torpedo_speed;
+    private float torpedo_speed;    // 魚雷の速度
+
+    private float brake = 3;   // 入力なし時の減速量
+    private float maxSpeed = 5;
 
     void Start()
     {
@@ -17,10 +20,32 @@ public class SubmarineMoveSample : MonoBehaviour
     {
         // `DataManager`に値をセット
         DataManager.SetSubmarinePosition(this.transform.position);
-        DataManager.SetSubmarineSpeed(speed);
-        DataManager.SetSubmarineDepth(depth);
+        //DataManager.SetSubmarineSpeed(speed);
+        //DataManager.SetSubmarineDepth(depth);
+
+        speed = DataManager.GetSubmarineSpeed();
+        depth = DataManager.GetSubmarineDepth();
+
+        streat();
     }
 
+
+
+    private void streat()
+    {
+        Vector3 move_direction = transform.forward * speed;
+        rigidbody.AddForce(move_direction, ForceMode.Impulse);
+
+        // 速度を少し減衰させる
+        rigidbody.linearVelocity = Vector3.MoveTowards(rigidbody.linearVelocity, Vector3.zero, brake * Time.fixedDeltaTime);
+
+        // 速度上限を設定
+        Vector3 v = rigidbody.linearVelocity;
+        if (v.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rigidbody.linearVelocity = v.normalized * maxSpeed;
+        }
+    }
 
     public void turn(float rotation_x)
     {
