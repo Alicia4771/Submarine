4a1a3d271389ab0783348cf8379987c99caa9996
Alicia4771 <rinn1204@icloud.com>
2025-11-24 14:28:19 +0900

    `SubmarineMoveSample.cs`を修正
    潜水艦が不意に止まるのを修正

--- SubmarineWar/Assets/Scripts/SubmarineMoveSample.cs
+++ SubmarineWar/Assets/Scripts/SubmarineMoveSample.cs
@@ -40,7 +40,7 @@ public class SubmarineMoveSample : MonoBehaviour
         if (speed != 0) rigidbody.AddForce(move_direction, ForceMode.Impulse);
 
         // 速度を少し減衰させる
-        rigidbody.linearVelocity = Vector3.MoveTowards(rigidbody.linearVelocity, Vector3.zero, brake * Time.fixedDeltaTime);
+        // rigidbody.linearVelocity = Vector3.MoveTowards(rigidbody.linearVelocity, Vector3.zero, brake * Time.fixedDeltaTime);
         // 強いブレーキ
         if (speed == 0) rigidbody.linearVelocity = Vector3.MoveTowards(rigidbody.linearVelocity, Vector3.zero, stop_brake * Time.fixedDeltaTime);
 
